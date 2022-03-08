ffbcf4b005fd932e9f4f8a84fae37cf3f797b3e9
irakli <irakli.khabeishvili.1@btu.edu.ge>
2022-03-08 23:21:19 +0400

    first commit

---
+++ ggg2.py
@@ -0,0 +1,100 @@
+# დავალება 1
+# class Calculator:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def gather(self, x, y):
+#         return x+y
+#
+#
+#     def subtraction(self, x, y):
+#         return  x-y
+#
+#     def devision(self, x, y):
+#         return x/y
+#
+#     def multiplication(self, x, y):
+#         return x*y
+#
+# a = int(input("შეიყვანეთ რიცხვი:"))
+# b = int(input("შეიყვანეთ რიცხვი:"))
+#
+# point = Calculator(a, b)
+# print("შეყვანილი რიცხვების ჯამია :",point.gather(a,b))
+# print("შეყვანილი რიცხვების სხვაობაა :",point.subtraction(a,b))
+# print("შეყვანილი რიცხვების განაყოფია :",point.devision(a,b))
+# print("შეყვანილი რიცხვების ნამრავლია :",point.multiplication(a,b))
+
+# დავალება 2
+# class Employee:
+#     def __init__(self, name, surname, age, salary):
+#         self.name = name
+#         self.surname = surname
+#         self.age = age
+#         self.salary = salary
+
+    #
+    # f=open("dataset1.csv","r")
+    # t=[]
+    # for i in f:
+    #     i=i.split(",")
+    #     t.append(i[0])
+    # print(t)
+    # f.close()
+    #
+    # f=open("dataset1.csv","r")
+    # y=[]
+    # for i in f:
+    #     i=i.split(",")
+    #     y.append(i[1])
+    # print(y)
+    # f.close()
+    #
+    # f=open("dataset1.csv","r")
+    # u=[]
+    # for i in f:
+    #     i=i.split(",")
+    #     u.append(i[2])
+    # print(u)
+    # f.close()
+    #
+    # f=open("dataset1.csv","r")
+    # o=[]
+    # for i in f:
+    #     i=i.split(",")
+    #     o.append(i[3])
+    # print(o)
+    # f.close()
+
+    # f = open("dataset1.csv","r")
+    # for i in f:
+    #     i=i.split(",")
+    #     i[3] = "1056"
+    #     print("Has the lowest salary : Austin,Olsen,63")
+    # f.close()
+    #
+    # f = open("dataset1.csv","r")
+    # for i in f:
+    #     i=i.split(",")
+    #     i[2] = "72 "
+    #     print("The oldest employee : Xena,Hobbs,72,    Rooney,Hensley,72")
+    # f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
